3eb62011daf8366c1b4ba4ab6aa6801c26723a1f
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-12-25 17:39:49 +0530

    wrote down query SQL

--- src/schemaExecutor.ts
+++ src/schemaExecutor.ts
@@ -1,12 +1,52 @@
 import pool from "./db";
 
 const schemaExecutor = async (log: boolean = false) => {
-  await pool.query(`CREATE TABLE IF NOT EXISTS users(
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS users(
         id SERIAL PRIMARY KEY,
         username TEXT NOT NULL,
         password TEXT NOT NULL,
         created_at TIMESTAMP DEFAULT NOW()
     )`);
+
+  await pool.query(`
+        CREATE TABLE IF NOT EXISTS participants(
+        id SERIAL PRIMARY KEY,
+        participant_name TEXT NOT NULL,
+        college_name TEXT NOT NULL,
+        department TEXT NOT NULL,
+        academic_year TEXT NOT NULL,
+        contact_number TEXT UNIQUE NOT NULL,
+        email TEXT UNIQUE NOT NULL,
+        created_at TIMESTAMP DEFAULT NOW()
+    )`);
+
+  await pool.query(`
+          CREATE TABLE IF NOT EXISTS event_days(
+          id SERIAL PRIMARY KEY,
+          day_name TEXT UNIQUE NOT NULL
+      )`);
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS events(
+      id SERIAL PRIMARY KEY,
+      event_name TEXT NOT NULL,
+      event_day_id INT REFERENCES event_days(id) ON DELETE CASCADE
+      )`);
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS registrations(
+      id SERIAL PRIMARY KEY,
+      participant_id INT NOT NULL REFERENCES participants(id) ON DELETE CASCADE,
+      day_id INT NOT NULL REFERENCES event_days(id) ON DELETE CASCADE,
+      registered_at TIMESTAMP DEFAULT NOW(),
+      UNIQUE(participant_id, day_id)
+  )`);
+
+  await pool.query(`CREATE TABLE IF NOT EXISTS registration_events(
+      id SERIAL PRIMARY KEY,
+      registration_id INT NOT NULL REFERENCES registrations(id) ON DELETE CASCADE,
+      event_id INT NOT NULL REFERENCES events(id) ON DELETE CASCADE,
+      UNIQUE(registration_id, event_id)
+  )`);
 
   if (log) {
     console.log("Query executed successfully");
