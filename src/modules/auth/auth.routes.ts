01624ba6df2255f9194c9d9643590117497ebd53
DharshanPalani <dharshanpalani.2006@gmail.com>
2026-01-14 19:45:25 +0530

    Added /user simple temp route

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { GoogleOAuth } from "../googleOAuth/googleOAuthCallBack";
+import type { Response } from "express";
 import passport from "passport";
 
 const googleOAuth = new GoogleOAuth();
@@ -17,5 +18,23 @@ authRouter.get(
   "/google/callback",
   googleOAuth.googleCallback.bind(googleOAuth),
 );
+
+authRouter.get("/user", (req: any, res: Response) => {
+  const user = req.user;
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  res.status(200).json({
+    id: user.id,
+    participant_name: user.participant_name,
+    email: user.email,
+    college_name: user.college_name,
+    department: user.department,
+    academic_year: user.academic_year,
+    contact_number: user.contact_number,
+  });
+});
 
 export default authRouter;
